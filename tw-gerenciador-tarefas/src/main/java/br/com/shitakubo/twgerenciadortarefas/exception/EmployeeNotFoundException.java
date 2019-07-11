5bf7b332545dfa77deac2b96fffa43f4fa269093
MARCELO SHITAKUBO <mshitakubo@ml.com>
2019-07-10 21:24:41 -0300

    gerenciador

---
+++ tw-gerenciador-tarefas/src/main/java/br/com/shitakubo/twgerenciadortarefas/exception/EmployeeNotFoundException.java
@@ -0,0 +1,4 @@
+package br.com.shitakubo.twgerenciadortarefas.exception;
+
+public class EmployeeNotFoundException extends Throwable {
+}
---
+++ twgerenciador/tw-gerenciador-tarefas/src/main/java/br/com/shitakubo/twgerenciadortarefas/TwGerenciadorTarefasApplication.java
@@ -0,0 +1,14 @@
+package br.com.shitakubo.twgerenciadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Controller
+public class TwGerenciadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwGerenciadorTarefasApplication.class, args);
+	}
+
+}
